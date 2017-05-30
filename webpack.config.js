39f51caf944ea119a12184dafaa7532f031fad41
Lauri Kinnunen <lkkinnunen@gmail.com>
2017-05-30 13:11:19 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './reservation/components/newreservation.js',
+
+    output: {
+        path: path.resolve(__dirname, './reservation/public/dist'),
+        filename: 'bundle.js'
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+
+                // Skip any files outside of your project's `src` directory
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+
+                // Only run `.js` and `.jsx` files through Babel
+                test: /\.jsx?$/,
+
+                // Options to configure babel with
+                query: {
+                    plugins: ['transform-runtime', 'transform-class-properties'],
+                    presets: ['react', 'stage-0', 'es2015']
+                }
+            }
+        ]
+    }
+};
